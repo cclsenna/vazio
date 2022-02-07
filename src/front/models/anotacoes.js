c49dbae5085ecda40aa118c8cf4854a0a3c836d9
Caio Senna <caiosenna93@gmail.com>
2022-02-07 10:14:45 -0300

    exercicio pronto

---
+++ src/front/models/anotacoes.js
@@ -0,0 +1,19 @@
+class Anotacoes{
+
+    constructor(titulo,notas){
+
+        if(typeof titulo=='string'&&typeof notas=='string'){
+            this.titulo=titulo;
+            this.notas=notas;
+        }
+        else{
+            alert('Criação de notas- dados inválidos');
+        }  
+
+    }
+
+
+
+    
+
+}
